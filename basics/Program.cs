d601d7aec084c2da1a69e5726f5a42a11585f82d
Pete Looney <petelooney81@gmail.com>
2020-12-11 12:19:51 -0600

    initial commit

---
+++ basics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace basics
+{
+    class Program
+    {
+        // Main is the application entry point, the method that's called automatically by the runtime when it launches the
+        // application. Any command line arguments supplied when the application is launched are available in the args array. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

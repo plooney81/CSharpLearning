acf4e0b95df7d17ac68ba67ea3f1811a1171c00e
Pete Looney <petelooney81@gmail.com>
2020-12-13 10:57:24 -0600

    madlib

---
+++ Madlib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Madlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity, story; // can declare multiple variables of the same type on the same line by adding commas in between
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            story = $"Roses are {color}\n{pluralNoun} are blue\nI Love {celebrity}";
+            Console.WriteLine(story);
+
+            Console.ReadLine();
+        }
+    }
+}

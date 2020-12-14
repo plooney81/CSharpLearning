9ef576c7ad52dbdac82c0a1592cc20a236c05dd6
Pete Looney <petelooney81@gmail.com>
2020-12-13 22:44:28 -0600

    if statements

---
+++ IfStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            bool isTall = true;
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            } else if(isMale)
+            {
+                Console.WriteLine("You are a short male");
+            } else if(isTall)
+            {
+                Console.WriteLine("You are a tall not male");
+            } else 
+            { 
+                Console.WriteLine("You are not male and short");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
